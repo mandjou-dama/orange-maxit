831e39a65489c180bc731a83f654b0257b0e6fb8
MozartOnWeb <mozartontheweb@gmail.com>
2025-10-26 23:57:29 +0000

    creating home component

---
+++ components/HomeMainCard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeMainCard = () => {
+  return (
+    <View>
+      <Text>HomeMainCard</Text>
+    </View>
+  )
+}
+
+export default HomeMainCard
